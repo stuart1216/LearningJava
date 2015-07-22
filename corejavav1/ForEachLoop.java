b57b36301a92a4148f1bbea4978fcc7a5a14a0f4
Adam Su <shqlyg@qq.com>
2015-07-22 09:55:24 +0800

    Reconstruction the floder

--- corejavav1/ForEachLoop.java
+++
@@ -1,8 +0,0 @@
-public class ForEachLoop{
-	public static void main(String args[]){
-		int [] a = new int[10];
-		for(int elment: a){
-			System.out.println(elment);
-		}
-	}
-}
--- corejavav1/OutputTest.java
+++
@@ -1,9 +0,0 @@
-public class OutputTest{
-	public static void main(String [] args){
-		String name = "Stuart";
-		int age = 32;
-		String message = String.format("%s, will be %d in next year.", "name", age);
-		System.out.println(message);
-		//System.out.printf("%tc",new Date());
-	}
-}
